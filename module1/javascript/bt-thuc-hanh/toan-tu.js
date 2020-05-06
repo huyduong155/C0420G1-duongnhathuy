5ef386f23abbf636a2ecdf93280edb79a13319d5
Your Name <nhathuy.155@gmail.com>
2020-05-07 00:04:27 +0700

    update

---
+++ module1/javascript/bt-thuc-hanh/toan-tu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth =prompt('enter the width')
+inputHeight =prompt('enter the height')
+let Width =parseInt(inputWidth);
+let height =parseInt(inputHeight);
+let area = Width * height;
+document.write('the area is:'+ area);
